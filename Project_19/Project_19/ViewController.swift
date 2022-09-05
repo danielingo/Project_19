9625fffd0e6ef5c9295ae6b3d52b998de246921d
Daniel Ingo <dingo3@gatech.edu>
2022-09-05 17:37:04 -0400

    Initial ARKit app

---
+++ Project_19/Project_19/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Project_19
+//
+//  Created by Daniel Ingo on 9/5/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
