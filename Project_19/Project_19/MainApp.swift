b4d467db94c5848f5b06b152e92e09fa88670ce8
fretw2 <draegonstorm@gmail.com>
2022-09-21 15:07:07 -0400

    initialization code for Firebase linking

--- Project_19/Project_19/MainApp.swift
+++ Project_19/Project_19/MainApp.swift
@@ -8,9 +8,22 @@
 import UIKit
 import RealityKit
 import SwiftUI
+// import FirebaseCore
+
+// class AppDelegate: NSObject, UIApplicationDelegate {
+//     func application(_ application: UIApplication, 
+//         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//             FirebaseApp.configure()
+            
+//             return true
+//         }
+// }
 
 @main
 struct MainApp: App {
+    // register app delegate for Firebase setup
+    // @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             NavigationView {
