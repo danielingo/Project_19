55707adaa839616def79316d5e0195464063fb46
Daniel Ingo <dingo3@gatech.edu>
2022-10-12 10:27:50 -0400

    Luke + Daniel pair programmed. Added File Manager.

--- Project_19/Project_19/Controllers/ARViewController.swift
+++ Project_19/Project_19/Controllers/ARViewController.swift
@@ -11,33 +11,60 @@ import RealityKit
 class ARViewController: UIViewController {
     @IBOutlet var arView: ARView!
 
+    //update to model obj
+    private var modelConfirmedForPlacement: Model?
+
+    private var models: [Model] =  {
+        let filemanager = FileManager.default
+
+        guard let path = Bundle.main.resourcePath, let
+            files = try?
+            filemanager.contentsOfDirectory(atPath: path) else {
+            return []
+        }
+        var availableModels: [Model] = []
+        for filename in files where filename.hasSuffix("usdz") {
+            let modelName = filename.replacingOccurrences(of: ".usdz", with: "")
+            let model = Model(modelName: modelName)
+            print("appending \(modelName)")
+            availableModels.append(model)
+        }
+        return availableModels
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let anchor = AnchorEntity()
-        anchor.position = simd_make_float3(0, -0.5, -1)
+        
+
+        
+       // let anchor = AnchorEntity()
+       // anchor.position = simd_make_float3(0, -0.5, -1)
 
         /*
         if let env1 = try? Entity.load(named: "env1") {
             anchor.addChild(env1)
         }
          */
+        modelConfirmedForPlacement = models[0]
         
-        if let env1 = try? Entity.load(named: "env1") {
-            
-        }
-        if let pokemart = try? Entity.load(named: "pokemart") {
-            anchor.addChild(pokemart)
-        } else {
-            print("DEBUG: pokemart did not load")
+        if let model = self.modelConfirmedForPlacement {
+            if let modelEntity = model.modelEntity {
+                //anchor.addChild(modelEntity)
+                print("DEBUD: Adding model to scene -> \(model.modelName)")
+                let anchorEntity = AnchorEntity()
+                anchorEntity.position = simd_make_float3(0, -0.5, -1)
+
+                anchorEntity.addChild(modelEntity)
+                arView.scene.addAnchor(anchorEntity)
+
+            } else {
+                print("Model no load")
+            }
         }
        
-        
-        
-        
-
-        arView.scene.anchors.append(anchor)
+        //arView.scene.anchors.append(anchor)
         
     }
     
