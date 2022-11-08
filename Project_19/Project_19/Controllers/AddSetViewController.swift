698bd9815ae9391f4fe62d14e8516906099c37ff
natashabohra425 <natsrbohra@gmail.com>
2022-11-08 18:48:41 -0500

    eyo cancel and done segues actually work, Natasha x Linsey dream team going hardddd

---
+++ Project_19/Project_19/Controllers/AddSetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddSetViewController.swift
+//  Project_19
+//
+//  Created by Natasha Bohra on 11/8/22.
+//
+
+import Foundation
+import UIKit
+import FirebaseFirestore
+
+class AddSetViewController: UITableViewController {
+    
+    var tag = "" {
+        didSet {
+            tagLabel.text = tag
+        }
+    }
+    
+    @IBOutlet weak var nameTextField : UITextField!
+    @IBOutlet weak var tagLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        tag = "Bubblegum"
+    }
+    
+}
---
+++ Project_19/Project_19/Controllers/TagPickerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TagPickerViewController.swift
+//  Project_19
+//
+//  Created by Natasha Bohra on 11/8/22.
+//
+
+import Foundation
+import UIKit
+
+class TagPickerViewController: UITableViewController {
+    
+//    let tagDataSource = TagDataSource()
+    
+}
